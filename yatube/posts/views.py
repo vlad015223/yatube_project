2bf3b64dd7f6de0111b7235ff22d6ab74478f120
Владислав <vlad01523@yandex.ru>
2022-07-02 14:36:25 +0300

    Успешно запустил свой первый сервер!

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Главная страница супер-проекта yatube')
+
+
+def group(request, any_slug):
+    return HttpResponse(f'Cтраница групп супер проекта {any_slug}')
