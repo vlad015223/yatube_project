65d6cfdd2ac6578f48719c647342ea9655be9445
Владислав <vlad01523@yandex.ru>
2022-07-05 18:32:25 +0300

    Что-то поправил, добавил ссылки в шаблоны

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index, name='main_posts'),
-    path('group/<slug:slug>/', views.group_posts, name='group_list')
+    path('', views.index, name='main_page'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
 ]
