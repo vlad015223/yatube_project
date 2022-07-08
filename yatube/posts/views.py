c84f561758f1ee0e66d355c01403d9b5b54e0159
Владислав <vlad01523@yandex.ru>
2022-07-08 13:47:15 +0300

    статики теперь работают, + добавил базу данных ORM

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from .models import Post, Group
 
 def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
     template = 'posts/index.html'
     context = {
-        'title': 'Это главная страница проекта Yatube',
-        'text': 'Последние обновления на сайте'
+        'posts': posts,
     }
     return render(request, template, context)
 
 
-def group_posts(request, any_slug):
-    return HttpResponse(f'Cтраница групп супер проекта {any_slug}')
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
