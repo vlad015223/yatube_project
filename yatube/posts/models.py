43c2611b0c6c58f43de3a60b423ea37bad45c42f
Владислав <vlad01523@yandex.ru>
2022-07-09 16:56:06 +0300

    Проект #2 готов!

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -24,5 +24,6 @@ class Group(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
     description = models.TextField()
+
     def __str__(self):
         return self.title
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-
 from .models import Post, Group
+
 
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
-    template = 'posts/index.html'
     context = {
         'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
